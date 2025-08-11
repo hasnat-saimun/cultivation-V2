25253e39c7a05e41d6ce4102ab40077b5de0a0e7
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2025-08-11 19:05:02 +0600

    Update sliderinfo.blade.php

--- resources/views/frontend/sliderinfo.blade.php
+++ resources/views/frontend/sliderinfo.blade.php
@@ -7,7 +7,7 @@
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
-                @if($sliderData)
+                @if($sliderData->count()>0)
                 @php
                     $sl = 1;
                 @endphp
