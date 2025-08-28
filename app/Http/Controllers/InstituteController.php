6fabf9e7f75b131248a8614570c027dc25b4bfeb
Md Abu Yousuf <46179916+vitprofessional@users.noreply.github.com>
2025-08-28 10:06:28 +0600

    Update InstituteController.php

--- app/Http/Controllers/InstituteController.php
+++ app/Http/Controllers/InstituteController.php
@@ -97,7 +97,7 @@ class InstituteController extends Controller
             $path = $dir.DIRECTORY_SEPARATOR.$name;
 
             // Read & resize (keeps aspect, prevents upscaling)
-            $img = Image::read($file)->contain(900, 450);
+            $img = Image::read($file)->cover(900, 450);
 
             // Encode with quality for lossy formats (PNG will ignore "quality")
             $binary = $img->encodeByExtension($ext, quality: 80);
