5e6632edcac4a4280b91ffd74ed3a94a209ac495
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2025-11-02 13:39:31 +0600

    Update MarksheetController.php

--- app/Http/Controllers/MarksheetController.php
+++ app/Http/Controllers/MarksheetController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Marksheet;
 use App\Models\newAdmission;
 use App\Models\GradeList;
-use App\Models\serverConfig;
+use App\Models\ServerConfig;
 
 class MarksheetController extends Controller
 {
