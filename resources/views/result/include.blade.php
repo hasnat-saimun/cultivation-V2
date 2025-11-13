f961ff0695f4d182f29abaab7a08355fcdeccb7b
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2025-11-13 15:25:57 +0600

    Update include.blade.php

--- resources/views/result/include.blade.php
+++ resources/views/result/include.blade.php
@@ -45,17 +45,7 @@
                     @endphp
                     <ul class="nav nav-sidebar-menu sidebar-toggle-view">
                         @if($userType == 1)
-                            <li class="nav-item sidebar-nav-item open" data-group="result-marks">
-                                <a href="#" class="nav-link active"><i class="flaticon-books"></i><span>Marks & Attendance</span></a>
-                                <ul class="nav sub-group-menu" style="display:block;">
-                                    <li class="nav-item">
-                                        <a href="{{ route('addMarks') }}" class="nav-link {{ request()->routeIs('addMarks') ? 'active' : '' }}"><i class="fas fa-angle-right"></i>Add Marks</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="{{ route('attendanceIndex') }}" class="nav-link {{ request()->routeIs('attendanceIndex') ? 'active' : '' }}"><i class="fas fa-angle-right"></i>Attendance</a>
-                                    </li>
-                                </ul>
-                            </li>
+                            @include('cultivation.teacherMenu')
                         @else
                             <li class="nav-item">
                                 <a href="{{ route('cultivationIndex') }}" class="nav-link {{ request()->routeIs('cultivationIndex') ? 'active' : '' }}"><i class="flaticon-dashboard"></i><span>Cultivation Panel</span></a>
