f1c57bd1e211c53615d0f1f740d2fb6e5ff42756
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2025-11-13 15:22:14 +0600

    Update teacherMenu.blade.php

--- resources/views/cultivation/teacherMenu.blade.php
+++ resources/views/cultivation/teacherMenu.blade.php
@@ -11,7 +11,7 @@
     </li>
     <li class="nav-item sidebar-nav-item {{ $marksOpen ? 'open' : '' }}" data-group="teacher-marks">
         <a href="#" class="nav-link {{ $marksOpen ? 'active' : '' }}"><i class="flaticon-books"></i><span>Marks Entry</span></a>
-        <ul class="nav sub-group-menu" style="display:block;">
+        <ul class="nav sub-group-menu" style="{{ $marksOpen ? 'display:block;' : '' }}">
             <li class="nav-item">
                 <a href="{{ route('addMarks') }}" class="nav-link {{ request()->routeIs('addMarks') ? 'active' : '' }}"><i class="fas fa-angle-right"></i>Add Marks</a>
             </li>
@@ -19,7 +19,7 @@
     </li>
     <li class="nav-item sidebar-nav-item {{ $attendanceOpen ? 'open' : '' }}" data-group="teacher-attendance">
         <a href="#" class="nav-link {{ $attendanceOpen ? 'active' : '' }}"><i class="fa-regular fa-calendar-check"></i><span>Attendance Management</span></a>
-        <ul class="nav sub-group-menu" style="display:block;">
+        <ul class="nav sub-group-menu" style="{{ $attendanceOpen ? 'display:block;' : '' }}">
             <li class="nav-item">
                 <a href="{{ route('attendanceIndex') }}" class="nav-link {{ request()->routeIs('attendanceIndex') ? 'active' : '' }}"><i class="fas fa-angle-right"></i>Mark Attendance</a>
             </li>
